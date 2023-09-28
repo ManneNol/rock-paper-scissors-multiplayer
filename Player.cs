93d95a4800729798cfbb21f07fc82603ec9f1c43
ManneNol <manuel@nodehill.com>
2023-09-28 11:16:00 +0200

    setup foundation for rock paper scissors
    added player class and option enum for input choice.
    setup player name input and initial game loop

---
+++ Player.cs
@@ -0,0 +1,12 @@
+namespace rock_paper_scissors_multiplayer;
+
+public class Player
+{
+    public readonly string Name;
+    public Option Choice;
+
+    public Player(string name)
+    {
+        Name = name;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using rock_paper_scissors_multiplayer;
 
-Console.WriteLine("Hello, World!");
+int playerCount = 2;
+List<Player> players = new List<Player>();
+
+for (int i = 1; i <= playerCount; i++)
+{
+    string name = string.Empty;
+    while (name.Length == 0)
+    {
+        Console.Clear();
+        Console.Write("Please enter the name of player " + i + ": ");
+        name = Console.ReadLine() ?? string.Empty;
+    }
+
+    players.Add(new Player(name));
+}
+
+for (int i = 1; i <= 3; i++)
+{
+    foreach (Player player in players)
+    {
+        Console.Clear();
+        Console.WriteLine("Round " + i);
+        Console.Write("Players in game: ");
+        Console.Write(player.Name + " ");
+    }
+
+
+    Console.ReadKey();
+}
